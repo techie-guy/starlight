253c74ba33e0aaa9f2d82fe4cc9592809f59cb1c
Techie Guy <devtechieguy@gmail.com>
2023-12-02 11:10:27 +0530

    Better Texture API

--- src/Texture.c
+++ src/Texture.c
@@ -1,42 +1,49 @@
 #include "Texture.h"
 #include "Utils.h"
 
-#if defined(_PLATFORM_NATIVE)
-	#include <glad/gles2.h>
-#elif defined(_PLATFORM_WEB)
-	#include <GLES3/gl3.h>
-#endif
-
 #include <stb_image.h>
 
-void initTexture(Texture* texture, const char* texturePath)
+void initTextureFromFile(Texture* texture, const char* texturePath)
+{
+	stbi_set_flip_vertically_on_load(true);
+
+	unsigned char* textureData = stbi_load(texturePath, &texture->width, &texture->height, &texture->channels, 0);
+	if(!textureData)
+	{
+		log_error("Invalid Texture Data: %s", texturePath);
+	}
+
+	initTextureFromData(texture, 0, GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE, textureData);
+
+	setTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	setTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	setTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	setTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	stbi_image_free(textureData);
+}
+
+void initTextureFromData(Texture* texture, GLint level, GLint internalformat, GLenum format, GLenum type, void* textureData)
 {
 	glGenTextures(1, &texture->textureId);
 	bindTexture(texture);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, level, internalformat, texture->width, texture->height, 0, format, type, textureData);	
+}
 
-	stbi_set_flip_vertically_on_load(true);
-
-	unsigned char* textureData = stbi_load(texturePath, &texture->width, &texture->height, &texture->channels, 0);
-	if(textureData)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture->width, texture->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData);
-	}
-	else
-	{
-		log_error("Couldn't load texture: %s", texturePath);
-	}
-
-	stbi_image_free(textureData);
+void setTextureParameteri(GLenum target, GLenum pname, GLint param)
+{
+	glTexParameteri(target, pname, param);
 }
 
 void bindTexture(Texture* texture)
 {
 	glBindTexture(GL_TEXTURE_2D, texture->textureId);
+}
+
+void unbindTexture()
+{
+	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 void destroyTexture(Texture* texture)
--- src/Texture.h
+++ src/Texture.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#if defined(_PLATFORM_NATIVE)
+	#include <glad/gles2.h>
+#elif defined(_PLATFORM_WEB)
+	#include <GLES3/gl3.h>
+#endif
+
 typedef struct
 {
 	unsigned int textureId;
@@ -8,6 +14,9 @@ typedef struct
 	int channels;
 } Texture;
 
-void initTexture(Texture* texture, const char* texturePath);
+void initTextureFromFile(Texture* texture, const char* texturePath);
+void initTextureFromData(Texture* texture, GLint level, GLint internalformat, GLenum format, GLenum type, void* textureData);
+void setTextureParameteri(GLenum target, GLenum pname, GLint param);
 void bindTexture(Texture* texture);
+void unbindTexture();
 void destroyTexture(Texture* texture);
