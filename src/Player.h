d10f30fe3131b2e3d6be5971757e4650079f9feb
Techie Guy <devtechieguy@gmail.com>
2023-12-27 00:55:52 +0530

    Remove Player.c and Player.h

--- src/Player.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cglm/struct.h>
-#include "Utils.h"
-
-void initPlayer(); 
-void updatePlayer(InputState inputState, float deltaTime);
-void drawPlayer(const mat4s viewTimesProj);
-void destroyPlayer();
