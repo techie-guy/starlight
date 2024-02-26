7e68c7d715b03cb28ed4c688f644ce4c7f8b0e70
Techie Guy <devtechieguy@gmail.com>
2024-02-26 18:36:42 +0530

    Update Camera

--- src/Camera.h
+++ src/Camera.h
@@ -3,9 +3,15 @@
 #include "Utils.h"
 #include <cglm/struct.h>
 
+enum CameraType
+{
+	NO_MOVE = 1 << 0,
+	LOOK_AROUND = 1 << 1,
+	WALK_AROUND = 1 << 2,
+};
+
 typedef struct
 {
-	vec2s window_dimensions;
 	mat4s projection_matrix;
 	mat4s view_matrix;
 
@@ -13,7 +19,20 @@ typedef struct
 	vec3s target;
 	vec3s up;
 
+	vec3s front;
+
 	float speed;
+	float mouse_sensitivity;
+	float fov;
+	float near_plane;
+	float far_plane;
+	float aspect_ratio;
+
+	// Rotation
+	float pitch;
+	float yaw;
+
+	enum CameraType camera_type;
 } Camera;
 
 void init_camera(Camera* camera);
