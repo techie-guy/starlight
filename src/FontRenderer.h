15bbd84993554ed72afeaab2549697094c4b9406
Techie Guy <devtechieguy@gmail.com>
2024-02-26 18:36:23 +0530

    Add Opacity Control to Font

--- src/FontRenderer.h
+++ src/FontRenderer.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cglm/struct.h>
-#include "Window.h"
 
-void init_font_renderer(const char* font_path, int character_size, Window* window);
-void render_text(char* text, float x, float y, float scale, vec3s color);
+void init_font_renderer(const char* font_path, int character_size);
+void render_text(char* text, float x, float y, float scale, char* hex_color, float opacity);
