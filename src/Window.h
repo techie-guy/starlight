1be5bcf66320b510de7b735b7db63cd0f9016457
Techie Guy <devtechieguy@gmail.com>
2024-02-26 18:32:46 +0530

    Add Mouse Callback

--- src/Window.h
+++ src/Window.h
@@ -3,10 +3,18 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <stdbool.h>
+#include <cglm/struct.h>
 
 typedef struct
 {
+	bool key_pressed;
 	bool key_pressed_data[GLFW_KEY_LAST];
+
+	bool mouse_moved;
+	vec2s mouse_position;
+
+	bool mouse_clicked;
+	bool mouse_clicked_data[GLFW_MOUSE_BUTTON_LAST];
 } InputSystem;
 
 typedef struct
@@ -23,6 +31,6 @@ void init_window(Window* window);
 int should_window_close(Window* window);
 void window_poll_events();
 void window_swap_buffers(Window* window);
-void change_window_color(char colorcode[7], float alpha);
+void change_window_color(vec4s color);
 void destroy_window(Window* window);
 
